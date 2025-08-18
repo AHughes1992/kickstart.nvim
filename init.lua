ae28da8b8aa41fd623e18de2eebeec07b828c0f2
Adam Hughes <ahughes1992@hotmail.com>
2025-08-18 22:58:26 +0100

    updated config

--- init.lua
+++ init.lua
@@ -209,6 +209,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
+--
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -633,35 +638,35 @@ require('lazy').setup({
           print 'Setting up roslyn bollocks'
         end,
         settings = {
-		  ['csharp|background_analysis'] = {
+          ['csharp|background_analysis'] = {
             dotnet_analyzer_diagnostics_scope = fullSolution,
             dotnet_compiler_diagnostics_scope = fullSolution,
           },
-		  ['csharp|symbol_search'] = {
-		    dotnet_search_reference_assemblies = true,
-		  },
+          ['csharp|symbol_search'] = {
+            dotnet_search_reference_assemblies = true,
+          },
           ['csharp|inlay_hints'] = {
             csharp_enable_inlay_hints_for_implicit_object_creation = true,
-			csharp_enable_inlay_hints_for_implicit_variable_types = true,
-			csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-			csharp_enable_inlay_hints_for_types = true,
-			dotnet_enable_inlay_hints_for_indexer_parameters = true,
-			dotnet_enable_inlay_hints_for_literal_parameters = true,
-			dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-			dotnet_enable_inlay_hints_for_other_parameters = true,
-			dotnet_enable_inlay_hints_for_parameters = true,
-			dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-			dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-			dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
           },
           ['csharp|code_lens'] = {
             dotnet_enable_references_code_lens = true,
           },
-		  ['csharp|completion'] = {
-		    dotnet_show_completion_items_from_unimported_namespaces = true,
-			dotnet_show_name_completion_suggestions = true,
-			dotnet_provide_regex_completions = true,
-		  }
+          ['csharp|completion'] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
+            dotnet_provide_regex_completions = true,
+          },
         },
       })
       -- Diagnostic Config
