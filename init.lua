732cd0898a17f75ad8662e5033fe106fdd74be77
Adam Hughes <ahughes1992@hotmail.com>
2025-09-05 22:52:43 +0100

    Config changes around LSP for dotnet

--- init.lua
+++ init.lua
@@ -291,6 +291,91 @@ require('lazy').setup({
     },
   },
 
+  {
+    'seblyng/roslyn.nvim',
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { 'cs', 'razor' },
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+
+    dependencies = {
+      {
+        -- By loading as a dependencies, we ensure that we are available to set
+        -- the handlers for Roslyn.
+        'tris203/rzls.nvim',
+        config = true,
+      },
+    },
+    lazy = false,
+    config = function()
+      -- Use one of the methods in the Integration section to compose the command.
+      local mason_registry = require 'mason-registry'
+
+      local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
+      local cmd = {
+        'roslyn',
+        '--stdio',
+        '--logLevel=Information',
+        '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
+        '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
+        '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
+        '--extension',
+        vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
+      }
+
+      vim.lsp.config('roslyn', {
+        cmd = cmd,
+        on_attach = function()
+          print 'Setting up Roslyn bollocks'
+        end,
+        handlers = require 'rzls.roslyn_handlers',
+        settings = {
+          ['csharp|background_analysis'] = {
+            dotnet_analyzer_diagnostics_scope = fullSolution,
+            dotnet_compiler_diagnostics_scope = fullSolution,
+          },
+          ['csharp|symbol_search'] = {
+            dotnet_search_reference_assemblies = true,
+          },
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+          ['csharp|completion'] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+            dotnet_show_name_completion_suggestions = true,
+            dotnet_provide_regex_completions = true,
+          },
+        },
+      })
+      vim.lsp.enable 'roslyn'
+    end,
+    init = function()
+      -- We add the Razor file types before the plugin loads.
+      vim.filetype.add {
+        extension = {
+          razor = 'razor',
+          cshtml = 'razor',
+        },
+      }
+    end,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -633,42 +718,6 @@ require('lazy').setup({
         end,
       })
 
-      vim.lsp.config('roslyn', {
-        on_attach = function()
-          print 'Setting up roslyn bollocks'
-        end,
-        settings = {
-          ['csharp|background_analysis'] = {
-            dotnet_analyzer_diagnostics_scope = fullSolution,
-            dotnet_compiler_diagnostics_scope = fullSolution,
-          },
-          ['csharp|symbol_search'] = {
-            dotnet_search_reference_assemblies = true,
-          },
-          ['csharp|inlay_hints'] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
-            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-            dotnet_enable_inlay_hints_for_literal_parameters = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-            dotnet_enable_inlay_hints_for_other_parameters = true,
-            dotnet_enable_inlay_hints_for_parameters = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-          },
-          ['csharp|code_lens'] = {
-            dotnet_enable_references_code_lens = true,
-          },
-          ['csharp|completion'] = {
-            dotnet_show_completion_items_from_unimported_namespaces = true,
-            dotnet_show_name_completion_suggestions = true,
-            dotnet_provide_regex_completions = true,
-          },
-        },
-      })
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
@@ -766,6 +815,11 @@ require('lazy').setup({
         registries = {
           'github:mason-org/mason-registry',
           'github:Crashdummyy/mason-registry',
+        },
+        ensure_installed = {
+          'csharpier',
+          --"roslyn",
+          'rzls',
         },
       }
       require('mason-lspconfig').setup {
@@ -993,7 +1047,25 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'hyprlang',
+        'vim',
+        'vimdoc',
+        'css',
+        'c_sharp',
+        'razor',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
